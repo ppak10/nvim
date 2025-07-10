efa17099436205abdc07047ea6753ffc7eab778f
Peter Pak <ppak10@gmail.com>
2025-07-10 16:48:51 -0400

    Updates to other config files.

--- init.lua
+++ init.lua
@@ -6,6 +6,29 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
 -- Override for python files only
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
