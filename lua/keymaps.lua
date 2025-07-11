18c6a66bc776eb32f0724ebf8bbc09abfde0eb87
Peter Pak <ppak10@gmail.com>
2025-07-11 12:12:43 -0400

    Removes molten and lazy loads obsidian.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -29,24 +29,6 @@ vim.keymap.set("n", "<leader>tt", ToggleTokyonightLightDark, { desc = "Toggle Li
 
 vim.keymap.set("i", "jj", "<ESC>")
 
--- Molten
-vim.keymap.set("n", "<leader>mi", ":MoltenInit<CR>",
-    { silent = true, desc = "Initialize the plugin" })
-vim.keymap.set("n", "<leader>e", ":MoltenEvaluateOperator<CR>",
-    { silent = true, desc = "run operator selection" })
-vim.keymap.set("n", "<leader>rl", ":MoltenEvaluateLine<CR>",
-    { silent = true, desc = "evaluate line" })
-vim.keymap.set("n", "<leader>rr", ":MoltenReevaluateCell<CR>",
-    { silent = true, desc = "re-evaluate cell" })
-vim.keymap.set("v", "<leader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
-    { silent = true, desc = "evaluate visual selection" })
-vim.keymap.set("n", "<leader>rd", ":MoltenDelete<CR>",
-    { silent = true, desc = "molten delete cell" })
-vim.keymap.set("n", "<leader>oh", ":MoltenHideOutput<CR>",
-    { silent = true, desc = "hide output" })
-vim.keymap.set("n", "<leader>os", ":noautocmd MoltenEnterOutput<CR>",
-    { silent = true, desc = "show/enter output" })
-
 -- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
